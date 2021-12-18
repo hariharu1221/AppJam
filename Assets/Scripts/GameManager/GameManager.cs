24b655774ef937fa5af399bca8acdb431edcef3c
hariharu1221 <jua.park1@gmail.com>
2021-12-18 17:47:49 +0900

    Setting_2

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static GameManager instance = null;
+    [SerializeField] GameObject P1;
+    [SerializeField] GameObject P2;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(instance);
+
+        if (P1 == null) P1 = GameObject.Find("P1");
+        if (P2 == null) P2 = GameObject.Find("P2");
+    }
+
+    private void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Object/Gem/Gem.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Gem : MonoBehaviour
+{
+    [SerializeField] SpawnForce spawnForce;
+    Rigidbody2D rigid;
+
+    void Start()
+    {
+        spawnForce.ForceX = UnityEngine.Random.Range(spawnForce.ForceXmin, spawnForce.ForceXmax); 
+        rigid = GetComponent<Rigidbody2D>();
+        WhenSpawnGem(spawnForce.ForceX,spawnForce.ForceY);
+        //Debug.Log(spawnForce.ForceX);
+    }
+
+    void WhenSpawnGem(float x,float y)
+    {
+        rigid.velocity = new Vector2(x, y);
+    }
+}
+
+[Serializable]
+public class SpawnForce
+{
+    public float ForceX;
+    [Range(-10,0)]
+    public float ForceXmin;
+    [Range(0,10)]
+    public float ForceXmax;
+    [Range(0,10)]
+    public float ForceY;
+}
